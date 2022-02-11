0290f37d1acc90dfc1fce2f67686e108d5f16fee
cristian <cristian.romero56@gmail.com>
2022-02-11 12:19:29 +0100

    primero

---
+++ ejercicio_indra/src/main/java/com/formacionspring/app/apirest/EjercicioIndraApplication.java
@@ -0,0 +1,13 @@
+package com.formacionspring.app.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioIndraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioIndraApplication.class, args);
+	}
+
+}
